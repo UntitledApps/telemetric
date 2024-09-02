bd2da23177973b175f4e02bacabaa1b81b848313
Karl <team@untitledapps.net>
2024-09-02 16:45:24 +0200

    chore: Exclude "supabase" from tsconfig.json and update eslint rules in .eslintrc.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { EmailForm } from "@/components/emailform";
-import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/server";
-import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
@@ -16,26 +14,22 @@ export default async function Home() {
         <div className="container mx-auto flex justify-between items-center py-4">
           {/* Logo and Name */}
           <div className="text-lg font-bold flex items-center">
-            <img src="/logo.png" alt="Logo" className="h-8 mr-2" />
+            <img src="/images/logo.png" alt="Logo" className="h-8 mr-2" />
             <span>Telemetric</span>
           </div>
           {/* Call to Action Button */}
-          <div className="flex items-center space-x-4">
-            <Button variant="outline">Join the waitlist</Button>
-          </div>
         </div>
       </nav>
 
       {/* Hero Section */}
       <section className="container mx-auto text-center py-16">
         <h1 className="text-4xl font-bold mb-4">
-          Finally a modern analytics platform
+          Analytics for modern entrepreneurs
         </h1>
         <p className="text-lg text-gray-600 max-w-2xl mx-auto mb-8">
-          Telemetric provides high-quality analytics tools for those who demand
-          excellence. Our platform offers seamless integration, advanced
-          visualizations, and privacy-focused features to empower your web apps
-          and projects.
+          Telemetric is a privacy-focused analytics platform for your apps, web
+          apps, and websites. Tired of all the mediocre analytics platforms out
+          there? Telemetric is here to change that.
         </p>
         <div className="flex justify-center space-x-4 mb-4">
           <EmailForm />
@@ -92,7 +86,6 @@ export default async function Home() {
       {/* Features Section */}
       <section className="container mx-auto flex flex-col gap-6 py-16 max-w-3xl">
         <div className="p-8 bg-white shadow-sm border border-gray-200 rounded-lg">
-          <Image src="/images/ss.png" width={100} height={100} alt="2" />
           <h3 className="font-semibold text-2xl mb-4">Privacy focused</h3>
           <p className="text-gray-600">
             Your users' data is anonymized and encrypted. No more worrying about
@@ -105,7 +98,7 @@ export default async function Home() {
             Well thought-through UI & UX
           </h3>
           <p className="text-gray-600">
-            Most analytics platforms are hard to use and overwhelming. We have
+            Most analytics platforms are hard to use and overwhelming. I have
             spent a lot of time making sure that the UI is easy to use and
             understand—and looks good.
           </p>
@@ -137,7 +130,7 @@ export default async function Home() {
       {/* Footer */}
       <footer className="bg-white py-6 border-t border-gray-200">
         <div className="container mx-auto text-center text-gray-500 text-sm">
-          <Link href="/privacy">Imprint & Privacy</Link>
+          <Link href="https://untitledapps.vercel.app">Imprint & Privacy</Link>
         </div>
         <div className="container mx-auto text-center text-gray-500 text-sm">
           &copy; 2024 UNTITLEDAPPS e.U. | All rights reserved.
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -26,7 +26,7 @@ export async function updateSession(request: NextRequest) {
           );
         },
       },
-    }
+    },
   );
 
   // IMPORTANT: Avoid writing any logic between createServerClient and
@@ -42,10 +42,6 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth")
   ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
